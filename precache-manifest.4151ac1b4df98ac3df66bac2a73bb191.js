b810f0dcdb98f03e2d821bb5dea69dab60f9546e
joseph.leach <joseph.leach@london.gov.uk>
2020-06-11 13:36:19 +0100

    Updates

---
+++ precache-manifest.4151ac1b4df98ac3df66bac2a73bb191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7d9f9cfd26bc77467a9",
+    "url": "/london-private-rent/static/css/main.ade42fa6.chunk.css"
+  },
+  {
+    "revision": "d7d9f9cfd26bc77467a9",
+    "url": "/london-private-rent/static/js/main.3f20b5d9.chunk.js"
+  },
+  {
+    "revision": "64d31416d8f5198bf747",
+    "url": "/london-private-rent/static/js/runtime~main.4719bff4.js"
+  },
+  {
+    "revision": "018ba1b5346a606ca617",
+    "url": "/london-private-rent/static/css/2.0af68281.chunk.css"
+  },
+  {
+    "revision": "018ba1b5346a606ca617",
+    "url": "/london-private-rent/static/js/2.aba8275a.chunk.js"
+  },
+  {
+    "revision": "19f3124bfa80d1fb1ea3675e51a53b92",
+    "url": "/london-private-rent/index.html"
+  }
+];
